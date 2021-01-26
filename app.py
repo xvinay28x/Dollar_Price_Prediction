fd6335e948ef1b23818d492f7ab5c3f0ae4a6899
Vinay Garg <xvinay28x@gmail.com>
2021-01-26 21:05:06 +0530

    -

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+
 model = pickle.load(open("model.pkl", "rb"))
 app = Flask(__name__)
-
 
 @app.route('/')
 def man():
